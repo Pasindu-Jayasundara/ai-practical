4f9ed421b623be426f07f1ec201c11a760f2fcf7
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2026-04-06 07:26:33 +0530

    neuron

--- pr-4/script.js
+++ pr-4/script.js
@@ -1,23 +1,37 @@
+let input1 = 1;
+let input2 = 1;
+let expectedOutput = 1;
+
+let weight1 = 0.6;
+let weight2 = 0.2;
+let bias = -2;
+
+let error = 0;
+
 function calculate() {
 
-    let input1 = 1;
-    let input2 = 1;
-    let expectedOutput = 1;
-
-    let weight1 = 0.6;
-    let weight2 = 0.2;
-    let bias = -2;
+    do{
 
     let weightedSum = (input1 * weight1) + (input2 * weight2) + bias;
 
-    let activationOutput = 1/1 + Math.exp(-weightedSum);
-    //console.log(activationOutput);
+    let activationOutput = 1 / (1 + Math.exp(-weightedSum));
+    console.log("Activation Output: " + activationOutput);
 
-    if(activationOutput >= 0.5) {
-        alert("Pass");
-    } else {
-        alert("Fail");  
-    }
 
-    let error = expectedOutput - activationOutput;
+    error = expectedOutput - activationOutput;
+    console.log("Error: " + error);
+    let learningRate = 0.5;
+
+    weight1 = weight1 + (learningRate * error * input1);
+    weight2 = weight2 + (learningRate * error * input2);
+    bias = bias + (learningRate * error);
+
+    console.log("Updated weight1: " + weight1);
+    console.log("Updated weight2: " + weight2);
+    console.log("Updated bias: " + bias);
+
+    // console.log("\n");
+    console.clear();
+    }while (error != 0) 
 }
+
